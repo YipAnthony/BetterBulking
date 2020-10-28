e6c250b407a25c1c30cb30de26137c10497ff7a1
Anthony Yip <anthonyjtyip@gmail.com>
2020-10-28 16:30:46 -0700

    Add meals via up/down arrows, each popup shows #of meals specific to selected recipe

--- src/Components/AddMealPopUp.js
+++ src/Components/AddMealPopUp.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {upArrow, downArrow} from './icons'
 
 export default function AddMealPopUp(props) {
 
@@ -25,109 +26,139 @@ export default function AddMealPopUp(props) {
         })
     }
 
+    function handleRemoveAdditionalMeal(e) {
+        let index = e.target.getAttribute('data-index')
+        let day = e.target.getAttribute('data-day')
+        let meal = props.mealToAdd
+        if (props.selectedMeals[index][day].length === 0) return
+
+        props.setSelectedMeals(prev => {
+            let output = [...prev]
+            let newOutput = []
+            
+            for (let j = 0; j < output[index][day].length; j ++) {
+                if (output[index][day][j]["title"] === meal["title"]) {
+                    newOutput = [...output[index][day].slice(0,j), ...output[index][day].slice(j+1)]
+                    output[index] = {...output[index], [day]:[...newOutput]}
+                    return output
+                }
+            }
+            
+        })
+    }
+
+
     let mealArray = []
     props.selectedMeals.forEach((value, index) => {
         
+        let breakfastCount = 0;
+        value["Breakfast"].forEach(item => {
+            if (item["title"] === props.mealToAdd["title"]) {
+                breakfastCount++;
+            }
+        })
+
+        let lunchCount = 0;
+        value["Lunch"].forEach(item => {
+            if (item["title"] === props.mealToAdd["title"]) {
+                lunchCount++;
+            }
+        })
+
+        let dinnerCount = 0;
+        value["Dinner"].forEach(item => {
+            if (item["title"] === props.mealToAdd["title"]) {
+                dinnerCount++;
+            }
+        })
+
+        let snackCount = 0;
+        value["Snack"].forEach(item => {
+            if (item["title"] === props.mealToAdd["title"]) {
+                snackCount++;
+            }
+        })
+
         mealArray.push(
             <div key={value["day"]} className="col dayOfWeekContainer">
                 <div className="dayOfWeek">{value["day"]}</div>
                 <div className="mealTime">
-                    {value["Breakfast"].length === 0 ? 
-                        <button 
-                            className="addButton btn btn-sm btn-outline-dark"
-                            data-index={index}
-                            data-day="Breakfast"
-                            onClick={handleAddRecipe}>Add</button>
-                        :
+                    
                         <div className="d-flex">
+                            <div className="mealCounter">{breakfastCount}</div>
                             <button 
                                 data-index={index}
                                 data-day="Breakfast"
-                                className="addButton btn btn-sm btn-outline-dark"
-                                onClick={handleAddAdditionalMeal}>Add</button>
+                                className="addButton btn btn-sm btn-outline-dark shadow-none"
+                                onClick={handleAddAdditionalMeal}>{upArrow}</button>
                             <button 
                                 data-index={index}
                                 data-day="Breakfast"
-                                className="addButton btn btn-sm btn-outline-dark"
-                                onClick={handleAddRecipe}>Replace</button>
+                                className="addButton btn btn-sm btn-outline-dark shadow-none"
+                                onClick={handleRemoveAdditionalMeal}>{downArrow}</button>
                         </div>
 
-                    }
+                    
                     {/* <div className="mealName">{Number(Object.keys(value["Breakfast"])) === 0 ? null: value["Breakfast"]["title"]}</div>
                     <div className="mealCal">{Number(Object.keys(value["Breakfast"])) === 0 ? null: value["Breakfast"]["calories"]}</div> */}
                 </div>
                 <div className="mealTime">
-                    {value["Lunch"].length  === 0 ? 
-                    <button 
-                            className="addButton btn btn-sm btn-outline-dark"
-                            data-index={index}
-                            data-day="Lunch"
-                            onClick={handleAddRecipe}>Add</button>
-                        :
+                   
                         <div className="d-flex">
+                            <div className="mealCounter">{lunchCount}</div>
                             <button 
-                                className="addButton btn btn-sm btn-outline-dark"
+                                className="addButton btn btn-sm btn-outline-dark shadow-none"
                                 data-index={index}
                                 data-day="Lunch"
                                 onClick={handleAddAdditionalMeal}
-                                >Add</button>
+                                >{upArrow}</button>
                             <button 
                                 data-index={index}
                                 data-day="Lunch"
-                                className="addButton btn btn-sm btn-outline-dark"
-                                onClick={handleAddRecipe}>Replace</button>
+                                className="addButton btn btn-sm btn-outline-dark shadow-none"
+                                onClick={handleRemoveAdditionalMeal}>{downArrow}</button>
                         </div>
-                    }
+                    
                     {/* <div className="mealName">{Number(Object.keys(value["Lunch"])) === 0 ? null: value["Lunch"]["title"]}</div>
                     <div className="mealCal">{Number(Object.keys(value["Lunch"])) === 0 ? null: value["Lunch"]["calories"]}</div> */}
                 </div>
                 <div className="mealTime">
-                    {value["Dinner"].length  === 0 ? 
-                    <button 
-                            className="addButton btn btn-sm btn-outline-dark"
-                            data-index={index}
-                            data-day="Dinner"
-                            onClick={handleAddRecipe}>Add</button>
-                        :
+                    
                         <div className="d-flex">
+                         <div className="mealCounter">{dinnerCount}</div> 
                             <button 
-                                className="addButton btn btn-sm btn-outline-dark"
+                                className="addButton btn btn-sm btn-outline-dark shadow-none"
                                 data-index={index}
                                 data-day="Dinner"
                                 onClick={handleAddAdditionalMeal}
-                                >Add</button>
+                                >{upArrow}</button>
                             <button 
                                 data-index={index}
                                 data-day="Dinner"
-                                className="addButton btn btn-sm btn-outline-dark"
-                                onClick={handleAddRecipe}>Replace</button>
+                                className="addButton btn btn-sm btn-outline-dark shadow-none"
+                                onClick={handleRemoveAdditionalMeal}>{downArrow}</button>
                         </div>
-                    }
+                    
                     {/* <div className="mealName">{Number(Object.keys(value["Dinner"])) === 0 ? null: value["Dinner"]["title"]}</div>
                     <div className="mealCal">{Number(Object.keys(value["Dinner"])) === 0 ? null: value["Dinner"]["calories"]}</div> */}
                 </div>
                 <div className="mealTime">
-                    {value["Snack"].length  === 0 ? 
-                    <button 
-                            className="addButton btn btn-sm btn-outline-dark"
-                            data-index={index}
-                            data-day="Snack"
-                            onClick={handleAddRecipe}>Add</button>
-                        :
+                   
                         <div className="d-flex">
+                            <div className="mealCounter">{snackCount}</div>
                             <button 
-                                className="addButton btn btn-sm btn-outline-dark"
+                                className="addButton btn btn-sm btn-outline-dark shadow-none"
                                 data-index={index}
                                 data-day="Snack"
                                 onClick={handleAddAdditionalMeal}
-                                >Add</button>
+                                >{upArrow}</button>
                             <button 
                                 data-index={index}
                                 data-day="Snack"
-                                className="addButton btn btn-sm btn-outline-dark"
-                                onClick={handleAddRecipe}>Replace</button>
+                                className="addButton btn btn-sm btn-outline-dark shadow-none"
+                                onClick={handleRemoveAdditionalMeal}>{downArrow}</button>
                         </div>
-                    }
+                    
                     {/* <div className="mealName">{Number(Object.keys(value["Dinner"])) === 0 ? null: value["Dinner"]["title"]}</div>
                     <div className="mealCal">{Number(Object.keys(value["Dinner"])) === 0 ? null: value["Dinner"]["calories"]}</div> */}
                 </div>
